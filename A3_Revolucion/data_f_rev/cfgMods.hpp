7a4cd861a79caf11d7584f4e71a9902cc10f0322
Ravenholme <61998820+Ravenholme@users.noreply.github.com>
2021-10-11 22:15:05 +0100

    Data_F_Rev populated

---
+++ A3_Revolucion/data_f_rev/cfgMods.hpp
@@ -0,0 +1,31 @@
+class CfgMods
+{
+	class Mod_Base;
+	class Revolucion: Mod_Base
+	{
+		picture = "\A3_Revolucion\Data_F_Rev\Logos\arma3_Revolucion_icon_CA.paa";
+		logo = "\A3_Revolucion\Data_F_Rev\Logos\arma3_Revolucion_logo_CA.paa";
+		logoOver = "\A3_Revolucion\Data_F_Rev\Logos\arma3_Revolucion_logoOver_CA.paa";
+		logoSmall = "\A3_Revolucion\Data_F_Rev\Logos\arma3_Revolucion_logo_small_CA.paa";
+		tooltip = $STR_A3_cfgMods_Revolucion_name0;
+		tooltipOwned = $STR_A3_cfgMods_Revolucion_name0;
+		action = linkRevolucion;
+		fieldManualTopicAndHint[] = {};
+		dlcColor[] =
+        {
+            0.58,   // R
+            0.82,   // G
+            0.89,   // B
+            1       // A
+        };
+		overview = $STR_A3_cfgMods_Revolucion_overview0;
+		logoTitle = "\A3_Revolucion\Data_F_Rev\Logos\arma3_Revolucion_logoTitle_CA.paa";
+		hideName = true;
+		hidePicture = false;
+		name = $STR_A3_cfgMods_Revolucion_name0;
+		author = $STR_A3_A_BranFlakes;
+		artwork = "\A3_Revolucion\Data_F_Rev\Logos\arma3_Revolucion_artwork.jpg";
+		overviewPicture = "\A3_Revolucion\Data_F_Rev\Images\Revolucion_overviewPicture_CO.paa";
+		dir = Revolucion;
+	};
+};
---
+++ A3_Revolucion/data_f_rev/cfgPatches.hpp
@@ -0,0 +1,16 @@
+class CfgPatches
+{
+    /* Definitions */
+    #include "\A3_Aegis\macros_urls.hpp"
+
+	class A3_Revolucion_Data_F_Rev
+	{
+		author = $STR_A3_A_BranFlakes;
+		name = "Arma 3 Revolución - Main Configuration";
+		url = linkRevolucion;
+		requiredAddons[] = {A3_Aegis_Data_F_Aegis_Loadorder};
+		requiredVersion = 0.1;
+		units[] = {/* Automated */};
+		weapons[] = {/* Automated */};
+	};
+};
