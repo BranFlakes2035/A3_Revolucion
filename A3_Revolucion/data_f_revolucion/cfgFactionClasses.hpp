2e07be1a07592d8a8ed675e7de5ccb9b48224fdf
Ravenholme <61998820+Ravenholme@users.noreply.github.com>
2021-10-13 22:54:35 +0100

    Faction Flag Placeholders

--- A3_Revolucion/data_f_revolucion/cfgFactionClasses.hpp
+++ A3_Revolucion/data_f_revolucion/cfgFactionClasses.hpp
@@ -1,11 +1,11 @@
 class CfgFactionClasses
 {
-	/*class Revolucion_BLU_BDF_F										 // BDF
+	class Revolucion_BLU_BDF_F										 // BDF
 	{
 		displayName = $STR_A3_R_CfgFactionClasses_BLU_BDF_F0;
 		priority = 3;
 		side = TWest;
-		//icon = "\A3_Revolucion\Data_F_Revolucion\FactionIcons\CfgFactionClasses_BLU_BDF_CA.paa";
+		icon = "\A3_Revolucion\Data_F_Revolucion\FactionIcons\CfgFactionClasses_BLU_BDF_CA.paa";
 		flag = "\A3_Revolucion\Data_F_Revolucion\Flags\flag_BDF_CO.paa";
 	};
 	class Revolucion_BLU_FRR_F										 // FRR BLUFOR
@@ -23,15 +23,15 @@ class CfgFactionClasses
 		side = TWest;
 		icon = "\A3\Data_F_Exp\FactionIcons\icon_CTRG_CA.paa";
 		flag = "\A3\Data_F_Exp\Flags\flag_CTRG_CO.paa";
-	}; 
-	/*class Revolucion_IND_FRR_F 										// FRR INDEP
+	};
+	class Revolucion_IND_FRR_F 										// FRR INDEP
 	{
 		displayName = $STR_A3_R_CfgFactionClasses_BLU_FRR_F0;
 		priority = 3;
 		side = TGuerrila;
 		icon = "\A3_Revolucion\Data_F_Revolucion\FactionIcons\CfgFactionClasses_BLU_FRR_CA.paa";
 		flag = "\A3_Revolucion\Data_F_Revolucion\Flags\flag_FRR_CO.paa";		
-	}; */
+	};
 	class Revolucion_IND_CDB_F 										// Cartels INDEP
 	{
 		displayName = $STR_A3_R_CfgFactionClasses_IND_CDB_F0;
@@ -47,13 +47,13 @@ class CfgFactionClasses
 		side = TGuerrila;
 		icon = "\A3_Revolucion\Data_F_Revolucion\FactionIcons\CfgFactionClasses_IND_BNP_CA.paa";
 		flag = "\A3_Revolucion\Data_F_Revolucion\Flags\flag_BNP_CO.paa";		
-	};
-	class Revolucion_OPF_F_F 										// FASR
+	}; */
+	class Revolucion_OPF_FASR_F 										// FASR
 	{
-		displayName = $STR_A3_R_CfgFactionClasses_OPF_F_F0;
+		displayName = $STR_A3_R_CfgFactionClasses_OPF_FASR_F0;
 		priority = 3;
 		side = TEast;
-		icon = "\A3_Revolucion\Data_F_Revolucion\FactionIcons\CfgFactionClasses_OPF_F_CA.paa";		
+		icon = "\A3_Revolucion\Data_F_Revolucion\FactionIcons\CfgFactionClasses_OPF_FASR_CA.paa";		
 		flag = "\A3_Revolucion\Data_F_Revolucion\Flags\flag_FASR_CO.paa";				
 	};
 	class Revolucion_OPF_FRR_F 										// FRR OPFOR
@@ -63,5 +63,5 @@ class CfgFactionClasses
 		side = TEast;
 		icon = "\A3_Revolucion\Data_F_Revolucion\FactionIcons\CfgFactionClasses_BLU_FRR_CA.paa";
 		flag = "\A3_Revolucion\Data_F_Revolucion\Flags\flag_FRR_CO.paa";			
-	};*/
+	};
 };
