b2f3a7c3c898c7251ec8b2a5806584cbf77ea50e
Ravenholme <61998820+Ravenholme@users.noreply.github.com>
2021-11-15 21:42:30 +0000

    BDF Offroad Fixes

--- A3_Revolucion/soft_f_revolucion/Offroad_01/cfgBDF.hpp
+++ A3_Revolucion/soft_f_revolucion/Offroad_01/cfgBDF.hpp
@@ -1,4 +1,4 @@
-class rev_B_B_Offroad_01_F: I_G_Offroad_01_F
+class Rev_B_B_Offroad_01_F: Offroad_01_military_base_F
 {
 	author = $STR_A3_A_BranFlakes;
 	//editorPreview = "\A3_Opf\EditorPreviews_F_Opf\Data\CfgVehicles\I_M_Offroad_01_F.jpg";
@@ -15,7 +15,7 @@ class rev_B_B_Offroad_01_F: I_G_Offroad_01_F
 		"\A3_revolucion\Soft_F_revolucion\Offroad_01\Data\Offroad_01_ext_BDF_CO.paa"
 	};
 };
-class rev_B_B_Offroad_01_armed_F: I_G_Offroad_01_armed_F
+class Rev_B_B_Offroad_01_armed_F: Offroad_01_armed_base_F
 {
 	author = $STR_A3_A_BranFlakes;
 	//editorPreview = "\A3_Opf\EditorPreviews_F_Opf\Data\CfgVehicles\I_M_Offroad_01_armed_F.jpg";
@@ -31,19 +31,8 @@ class rev_B_B_Offroad_01_armed_F: I_G_Offroad_01_armed_F
 		"\A3_revolucion\Soft_F_revolucion\Offroad_01\Data\Offroad_01_ext_BDF_CO.paa",
 		"\A3_revolucion\Soft_F_revolucion\Offroad_01\Data\Offroad_01_ext_BDF_CO.paa"
 	};
-	class AnimationSources: AnimationSources
-	{
-		class Hide_Shield: Hide_Shield
-		{
-			initPhase = false;
-		};
-		class Hide_Rail: Hide_Rail
-		{
-			initPhase = false;
-		};
-	};
 };
-class rev_B_B_Offroad_01_covered_F: Offroad_01_military_covered_base_F
+class Rev_B_B_Offroad_01_covered_F: Offroad_01_military_covered_base_F
 {
 	author = $STR_A3_A_BranFlakes;
 	//editorPreview = "\A3_Opf\EditorPreviews_F_Opf\Data\CfgVehicles\I_M_Offroad_01_covered_F.jpg";
@@ -69,8 +58,36 @@ class rev_B_B_Offroad_01_covered_F: Offroad_01_military_covered_base_F
 		"\A3_revolucion\Soft_F_revolucion\Offroad_01\Data\Offroad_01_ext_BDF_CO.paa",
 		"\A3_revolucion\Soft_F_revolucion\Offroad_01\Data\Offroad_01_cover_BDF_CO.paa"
 	};
+
+	/* Scripts */
 	class EventHandlers: EventHandlers
 	{
 		init = "if (local (_this select 0)) then {[(_this select 0), """", [], true] call bis_fnc_initVehicle;};";
+	};
+};
+class Rev_B_B_Offroad_01_comms_F: Offroad_01_military_comms_base_F
+{
+	author = $STR_A3_A_Ravenholme;
+	//editorPreview = "\A3_Revolucion\EditorPreviews_F_Revolucion\Data\CfgVehicles\Rev_B_B_Offroad_comms_01_F.jpg";
+	scope = public;
+	scopeCurator = public;
+	accuracy = 1.25;
+	side = TWest;
+	faction = Revolucion_BLU_BDF_F;
+	crew = Rev_B_BDF_Soldier_F;
+	typicalCargo[] = {Rev_B_BDF_Soldier_F};
+    textureList[] = {BDF,1};
+	animationList[] =
+	{
+        HideCover,0,
+        HideDoor3,0,
+        HideBumper1,1,
+        HideBumper2,0
+	};
+	hiddenSelectionsTextures[] =
+	{
+		"\A3_revolucion\Soft_F_revolucion\Offroad_01\Data\Offroad_01_ext_BDF_CO.paa",
+		"\A3_revolucion\Soft_F_revolucion\Offroad_01\Data\Offroad_01_ext_BDF_CO.paa",
+		"\A3_revolucion\Soft_F_revolucion\Offroad_01\Data\Offroad_01_cover_BDF_CO.paa"
 	};
 };
---
+++ A3_Revolucion/soft_f_revolucion/Offroad_01/cfgVehicles.hpp
@@ -0,0 +1,59 @@
+class CfgVehicles
+{
+	// Arma 3
+	class Car_F;
+	class Offroad_01_base_F: Car_F{};
+    class Offroad_01_military_base_F: Offroad_01_base_F
+    {
+        class AnimationSources;
+		class TextureSources
+		{
+			class BDF
+			{
+				displayName = "BDF";
+				author = $STR_A3_A_BranFlakes;
+				textures[] =
+				{
+					"\A3_revolucion\Soft_F_revolucion\Offroad_01\Data\Offroad_01_ext_BDF_CO.paa",
+					"\A3_revolucion\Soft_F_revolucion\Offroad_01\Data\Offroad_01_ext_BDF_CO.paa"
+				};
+				materials[] =
+				{
+					"\A3_Aegis\Soft_F_Aegis\Offroad_01\Data\Offroad_01_ext_IG.rvmat",
+					"\A3\Soft_F\Offroad_01\Data\Offroad_01_ext_plastic.rvmat"
+				};
+				faction[] = {Revolucion_BLU_BDF_F};
+			};
+		};
+    };
+    class Offroad_01_armed_base_F: Offroad_01_military_base_F{};
+    class Offroad_01_military_covered_base_F: Offroad_01_military_base_F
+    {
+        class TextureSources
+        {
+            class BDF
+			{
+				displayName = "BDF";
+				author = $STR_A3_A_BranFlakes;
+				textures[] =
+				{
+					"\A3_revolucion\Soft_F_revolucion\Offroad_01\Data\Offroad_01_ext_BDF_CO.paa",
+					"\A3_revolucion\Soft_F_revolucion\Offroad_01\Data\Offroad_01_ext_BDF_CO.paa",
+                    "\A3_revolucion\Soft_F_revolucion\Offroad_01\Data\Offroad_01_cover_BDF_CO.paa"
+				};
+				materials[] =
+				{
+					"\A3_Aegis\Soft_F_Aegis\Offroad_01\Data\Offroad_01_ext_IG.rvmat",
+					"\A3\Soft_F\Offroad_01\Data\Offroad_01_ext_plastic.rvmat"
+				};
+				faction[] = {Revolucion_BLU_BDF_F};
+			};
+        };
+        class EventHandlers;
+    };
+    class Offroad_01_military_comms_base_F: Offroad_01_military_covered_base_F{};
+
+    
+	// Arma 3 Revolucion
+	#include "cfgBDF.hpp"               // BDF
+};
