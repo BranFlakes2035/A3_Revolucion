2a68b6b4e07832689ea01d71f036fcf17586f2fc
BranFlakes2035 <78566121+BranFlakes2035@users.noreply.github.com>
2021-11-21 22:41:36 -0800

    FASR M81 Radio Bag

--- A3_Revolucion/supplies_f_revolucion/Bags/cfgVehicles.hpp
+++ A3_Revolucion/supplies_f_revolucion/Bags/cfgVehicles.hpp
@@ -10,6 +10,24 @@ class CfgVehicles
     class B_Kitbag_rgr;
     class B_Kitbag_blk;
     class B_AssaultPack_khk;
+    class B_RadioBag_01_base_F;
+
+    class B_RadioBag_01_aucamo_F: B_RadioBag_01_base_F
+    {
+        author = $STR_A3_A_BranFlakes;
+        scope = public;
+        displayName = "Radiobag (M81)";
+        picture = "\a3\supplies_f_enoch\Bags\Data\UI\icon_B_RadioBag_01_wdl_F_ca.paa";
+        hiddenSelectionsTextures[] = {"\A3_revolucion\Supplies_F_revolucion\Bags\Data\B_RadioBag_01_m81_CO.paa"};
+
+        /* TFAR */
+        tf_dialog = bussole_radio_dialog;
+        tf_dialogUpdate = "call TFAR_fnc_updateLRDialogToChannel;";
+        tf_encryptionCode = tf_east_radio_code;
+        tf_hasLRradio = true;
+        tf_range = 20000;
+        tf_subtype = digital_lr;
+    };
 
     /* Factions */
     #include "cfgCartels.hpp"   // Cartel De Baracas
