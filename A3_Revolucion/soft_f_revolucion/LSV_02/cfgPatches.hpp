3847675bfa24b3a14c71aee6e7dd9b176e19868b
BranFlakes2035 <78566121+BranFlakes2035@users.noreply.github.com>
2021-12-05 17:16:08 -0800

    VZ Quilin

---
+++ A3_Revolucion/soft_f_revolucion/LSV_02/cfgPatches.hpp
@@ -0,0 +1,18 @@
+class CfgPatches
+{
+	class A3_revolucion_Soft_F_revolucion_LSV_02
+	{
+		addonRootClass = A3_revolucion_Soft_F_revolucion;
+		requiredAddons[] = {A3_revolucion_Soft_F_revolucion};
+		requiredVersion = 0.1;
+		units[] =
+        {
+            // Venezuela
+            revolucion_O_VZ_LSV_02_armed_F,
+            revolucion_O_VZ_LSV_02_armed_viper_F,
+            revolucion_O_VZ_LSV_02_AT_F,
+            revolucion_O_VZ_LSV_02_unarmed_F
+        };
+		weapons[] = {/* Auto-compiled by pboProject */};
+	};
+};
