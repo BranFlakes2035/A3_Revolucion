5bfa96d05e1cca9236cf10036a9aa11853e86b34
BranFlakes2035 <78566121+BranFlakes2035@users.noreply.github.com>
2021-12-06 21:50:35 -0800

    Icon Fluff

--- A3_Revolucion/data_f_revolucion/cfgMods.hpp
+++ A3_Revolucion/data_f_revolucion/cfgMods.hpp
@@ -1,4 +1,4 @@
-/*class CfgMods
+class CfgMods
 {
 	class Mod_Base;
 	class Revolucion: Mod_Base
@@ -7,8 +7,8 @@
 		logo = "\A3_Revolucion\Data_F_Revolucion\Logos\arma3_Revolucion_logo_CA.paa";
 		logoOver = "\A3_Revolucion\Data_F_Revolucion\Logos\arma3_Revolucion_logoOver_CA.paa";
 		logoSmall = "\A3_Revolucion\Data_F_Revolucion\Logos\arma3_Revolucion_logo_small_CA.paa";
-		tooltip = $STR_A3_cfgMods_Revolucion_name0;
-		tooltipOwned = $STR_A3_cfgMods_Revolucion_name0;
+		tooltip = $STR_A3_R_CfgMods_Revolucion_name0;
+		tooltipOwned = $STR_A3_R_CfgMods_Revolucion_name0;
 		action = linkRevolucion;
 		fieldManualTopicAndHint[] = {};
 		dlcColor[] =
@@ -18,14 +18,14 @@
             0.89,   // B
             1       // A
         };
-		overview = $STR_A3_cfgMods_Revolucion_overview0;
+		overview = $STR_A3_R_CfgMods_Revolucion_overview0;
 		logoTitle = "\A3_Revolucion\Data_F_Revolucion\Logos\arma3_Revolucion_logoTitle_CA.paa";
 		hideName = true;
 		hidePicture = false;
-		name = $STR_A3_cfgMods_Revolucion_name0;
+		name = $STR_A3_R_CfgMods_Revolucion_name0;
 		author = $STR_A3_A_BranFlakes;
 		artwork = "\A3_Revolucion\Data_F_Revolucion\Logos\arma3_Revolucion_artwork.jpg";
 		overviewPicture = "\A3_Revolucion\Data_F_Revolucion\Images\Revolucion_overviewPicture_CO.paa";
 		dir = Revolucion;
 	};
-};*/
+};
