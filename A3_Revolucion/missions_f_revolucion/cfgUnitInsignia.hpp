e139609f9113c98cdf763cc67748239153754192
BranFlakes2035 <78566121+BranFlakes2035@users.noreply.github.com>
2021-12-10 21:03:08 -0800

    Group 9 Patch

--- A3_Revolucion/missions_f_revolucion/cfgUnitInsignia.hpp
+++ A3_Revolucion/missions_f_revolucion/cfgUnitInsignia.hpp
@@ -4,7 +4,7 @@ class CfgUnitInsignia
 	{
 		displayName = "CTRG 9 - NATO";		            						            // Name displayed in Arsenal
 		author = "TheMightySultan";	    										            // Author displayed in Arsenal
-		texture = "\A3_revolucion\ui_f_revolucion\data\UnitInsignia\group9.paa";			// Image path
+		texture = "\A3_revolucion\ui_f_revolucion\data\UnitInsignia\CTRG_Gr9_CA.paa";			// Image path
 		material = "\A3\Ui_f\data\GUI\Cfg\UnitInsignia\default_insignia.rvmat";         	// .rvmat path
 		textureVehicle = "";													            // Does nothing, reserved for future use
 	};
