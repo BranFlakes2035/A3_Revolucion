34f31ac2b5fef6e2b0eb3870d66bcd5540b7b739
BranFlakes2035 <78566121+BranFlakes2035@users.noreply.github.com>
2022-01-05 17:55:17 -0800

    Diversified CTRG Group 9's Blood Types

--- A3_Revolucion/characters_f_revolucion/Vests/cfgWeapons.hpp
+++ A3_Revolucion/characters_f_revolucion/Vests/cfgWeapons.hpp
@@ -31,6 +31,7 @@ class CfgWeapons
 		displayName = $STR_A3_A_CfgWeapons_V_CarrierRigKBT_01_ctrg_F0;
 		picture = "\A3_Revolucion\Characters_F_Revolucion\Vests\Data\UI\V_CarrierRigKBT_01_ctrg_F_CA.paa";
 		hiddenSelectionsTextures[] = {"\A3_Revolucion\Characters_F_Revolucion\Vests\Data\CarrierRigKBT_01_CTRG_CO.paa"};
+		hiddenSelectionsMaterials[] = {"\A3_Revolucion\Characters_F_Revolucion\Vests\Data\CarrierRigKBT_01_CTRG.rvmat"};
     };
     class V_CarrierRigKBT_01_light_ctrg_F: V_CarrierRigKBT_01_light_base_F
 	{
@@ -39,6 +40,7 @@ class CfgWeapons
 		displayName = $STR_A3_A_CfgWeapons_V_CarrierRigKBT_01_light_ctrg_F0;
 		picture = "\A3_Revolucion\Characters_F_Revolucion\Vests\Data\UI\V_CarrierRigKBT_01_light_ctrg_F_CA.paa";
 		hiddenSelectionsTextures[] = {"\A3_Revolucion\Characters_F_Revolucion\Vests\Data\CarrierRigKBT_01_CTRG_CO.paa"};
+		hiddenSelectionsMaterials[] = {"\A3_Revolucion\Characters_F_Revolucion\Vests\Data\CarrierRigKBT_01_CTRG.rvmat"};
     };
     class V_CarrierRigKBT_01_heavy_ctrg_F: V_CarrierRigKBT_01_heavy_base_F
 	{
@@ -47,6 +49,7 @@ class CfgWeapons
 		displayName = $STR_A3_A_CfgWeapons_V_CarrierRigKBT_01_heavy_ctrg_F0;
 		picture = "\A3_Revolucion\Characters_F_Revolucion\Vests\Data\UI\V_CarrierRigKBT_01_heavy_ctrg_F_CA.paa";
 		hiddenSelectionsTextures[] = {"\A3_Revolucion\Characters_F_Revolucion\Vests\Data\CarrierRigKBT_01_CTRG_CO.paa"};
+		hiddenSelectionsMaterials[] = {"\A3_Revolucion\Characters_F_Revolucion\Vests\Data\CarrierRigKBT_01_CTRG.rvmat"};
     };
     /*class V_CarrierRigKBT_01_whex_F: V_CarrierRigKBT_01_base_F
 	{
