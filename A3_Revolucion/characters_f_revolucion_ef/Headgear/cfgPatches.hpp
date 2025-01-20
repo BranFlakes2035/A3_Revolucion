df20c361e5f934dd4ccda0ec40333ff7b0cfccc7
Ravenholme <61998820+Ravenholme@users.noreply.github.com>
2025-01-20 16:11:48 +0000

    Fix for circular dependency when EF isn't loaded

--- A3_Revolucion/characters_f_revolucion_ef/Headgear/cfgPatches.hpp
+++ A3_Revolucion/characters_f_revolucion_ef/Headgear/cfgPatches.hpp
@@ -1,10 +1,11 @@
 class CfgPatches
 {
-	class A3_revolucion_Characters_F_revolucion_Headgear_ef
+	class A3_Revolucion_Characters_F_Revolucion_Headgear_EF
 	{
-		addonRootClass = A3_Revolucion_Characters_F_Revolucion_ef;
-		requiredAddons[] = {A3_Revolucion_Characters_F_Revolucion_ef};
+		addonRootClass = A3_Revolucion_Characters_F_Revolucion_EF;
+		requiredAddons[] = {A3_Revolucion_Characters_F_Revolucion_EF};
 		requiredVersion = 0.1;
+		skipWhenMissingDependencies = 1;
 		units[] = {/* Auto-compiled by pboProject */};
 		weapons[] = {/* Auto-compiled by pboProject */};
 	};
